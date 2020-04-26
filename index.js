a5a27fbed69f4307f5b2e3ab3d67cf24fa6e097d
kJ <kajitam714@gmail.com>
2020-04-26 11:53:40 +0900

    reply implove

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
 // -----------------------------------------------------------------------------
-// ���W���[���̃C���|�[�g
+// モジュールのインポート
 const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging API��SDK���C���|�[�g
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 // -----------------------------------------------------------------------------
-// �p�����[�^�ݒ�
+// パラメータ設定
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // ���ϐ�����A�N�Z�X�g�[�N�����Z�b�g���Ă��܂�
-    channelSecret: process.env.LINE_CHANNEL_SECRET // ���ϐ�����Channel Secret���Z�b�g���Ă��܂�
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
 };
 
 // -----------------------------------------------------------------------------
-// Web�T�[�o�[�ݒ�
+// Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
 
+// APIコールのためのクライアントインスタンスを作成
+const bot = new line.Client(line_config);
+
 // -----------------------------------------------------------------------------
-// ���[�^�[�ݒ�
+// ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
     res.sendStatus(200);
-    console.log(req.body);
+
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event) => {
+
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "こんにちは"){                
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
+            }else{
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "cogito, ergo sum.\n我思う、故に我あり。"
+                }));
+            }
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
